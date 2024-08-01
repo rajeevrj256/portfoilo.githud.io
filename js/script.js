3c0f6bcf5d33ed58272b950125c8a8c14887cfd7
rajeevrj256 <rjrajeev5918@gmail.com>
2024-08-01 23:47:55 +0530

    new update

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
 
 
 const typed= new Typed('.multiple-text',{
-    strings: ['Frontend  Developer','Backend Developer', 'c++ programmer'],
+    strings: ['Frontend developer','React Developer', 'c++ programmer','Mern stack developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
